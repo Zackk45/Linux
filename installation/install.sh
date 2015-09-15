41ff5df989e7867bdaa412415d1e4cdfa326c0fd
Zackk45 <cyril.conde@gmail.com>
2015-09-15 23:18:59 +0200

    Grosse modif backup et install.sh

--- installation/install.sh
+++ installation/install.sh
@@ -1,40 +1,47 @@
 #! /bin/bash
 #
-###############################################################################
-##
-##
-##
-##
-##
-##
-##
-##
-##
-##
-###############################################################################
-#
-#
+##################################################################################################
+## 1.Télécharger le script																		##
+##		wget https://raw.githubusercontent.com/Zackk45/Linux/master/installation/ 				##
+## 2.Le rendre éxecutable																		##
+##		chmod +x ./install.sh																	##
+## 3.L'éxecuter																					##
+##		./install.sh																			##
+##################################################################################################
 
 cd $HOME
 
 #Installation de l'environnement graphique openbox + outils
-sudo apt-get install -y xorg openbox xinit pcmanfm chromium-browser terminator notify-osd libnotify-bin numlockx
-sudo apt-get install -y libc-dev-bin libc6-dev linux-libc-dev manpages manpages-fr manpages-fr-extra manpages-fr-dev manpages-dev manpages-posix-dev xscreensaver xscreensaver-data ibus ibus-gtk ibus-gtk3 libibus-1.0 poppler-utils wkhtmltopdf
+sudo apt-get install xorg openbox xinit pcmanfm chromium-browser terminator notify-osd libnotify-bin numlockx
+sudo apt-get update
+sudo apt-get install libc-dev-bin libc6-dev linux-libc-dev manpages manpages-fr manpages-fr-extra manpages-fr-dev manpages-dev manpages-posix-dev xscreensaver xscreensaver-data ibus ibus-gtk ibus-gtk3 libibus-1.0 poppler-utils wkhtmltopdf
+sudo apt-get update
+sudo apt-get install lubuntu-restricted-addons lubuntu-restricted-extras linux-firmware-nonfree
+sudo apt-get update
 
 #Installation de logiciel complémentaire
-sudo apt-get install -y obconf lxappearance obmenu xarchiver alsa-utils gnome-alsamixer wicd menu menu-xdg gnome-menus openbox-xdgmenu gpicview cups elementary-icon-theme nitrogen conky plank tint2 gufw compton deborphan synaptic apturl gdebi htop gmrun vim lubuntu-restricted-addons lubuntu-restricted-extras linux-firmware-nonfree scrot gpicview evince gedit gksu vlc lubuntu-software-center language-selector-gnome tree ppa-purge git
+sudo apt-get install obconf lxappearance obmenu xarchiver alsa-utils gnome-alsamixer wicd menu menu-xdg gnome-menus openbox-xdgmenu gpicview cups
+sudo apt-get update
+sudo apt-get install elementary-icon-theme nitrogen conky plank tint2 gufw compton deborphan synaptic apturl gdebi htop gmrun
+sudo apt-get update
+sudo apt-get install vim scrot gpicview evince gedit gksu vlc lubuntu-software-center language-selector-gnome tree ppa-purge git libreoffice-common
+sudo apt-get update
 
 #Instatallation de codec vidéo/audio + format de compression
-sudo apt-get install -y unace rar unrar unar p7zip-rar p7zip zip unzip arj libuu0 mpack sharutils uudeview gstreamer0.10-plugins-ugly gxine libdvdread4 totem-mozilla icedax tagtool easytag id3tool lame libmad0 mpg321 libavcodec-extra xz-utils
+sudo apt-get install unace rar unrar unar p7zip-rar p7zip zip unzip arj libuu0 mpack sharutils uudeview gstreamer0.10-plugins-ugly gxine libdvdread4 icedax tagtool easytag id3tool lame libmad0 mpg321 libavcodec-extra xz-utils
+sudo apt-get update
 
 #Installation du microcode pour les processeurs intel
-sudo apt-get install -y intel-microcode
+sudo apt-get install intel-microcode
+sudo apt-get update
 
 #Gestion de l'énergie, ajout du dépôt et installation
 sudo apt-add-repository ppa:linrunner/tlp
 sudo apt-get update
-sudo apt-get install -y tlp tlp-rdw
-sudo apt-get remove -y --purge network-manager-gnome
+sudo apt-get install tlp tlp-rdw
+sudo apt-get update
+sudo apt-get remove --purge network-manager-gnome
+sudo apt-get update
 
 #Ajout de l'utilisateur aux groupe audio pour faire fonctionner le son et netdev pour internet
 sudo adduser $USER audio
@@ -58,8 +65,9 @@ echo vm.vfs_cache_pressure=50 | sudo tee -a /etc/sysctl.d/99-swappiness.conf
 sudo sysctl -p /etc/sysctl.d/99-swappiness.conf
 
 #Mise à jour, nettoyage du système et redémarrage
-sudo apt-get dist-upgrade -y
-sudo apt-get autoremove -y
+sudo apt-get dist-upgrade
+sudo apt-get autoremove
 telinit 6
+exit 0
 
 #Modifier la méthode d'entré au clavier (gksudo ibus-setup) ainsi que la langue (gksudo /usr/bin/gnome-language-selector)
