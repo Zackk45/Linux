23229aaacdcf68b2ff16b54af971ce2c2cee6de5
Zackk45 <cyril.conde@gmail.com>
2016-03-23 13:46:01 +0100

    Nouvelle version

--- installation/install.sh
+++ installation/install.sh
@@ -13,7 +13,6 @@
 cd $HOME
 
 #Installation de l'environnement graphique openbox + outils
-<<<<<<< HEAD
 sudo apt-get install - y xorg openbox xinit pcmanfm chromium-browser terminator notify-osd libnotify-bin numlockx
 sudo apt-get update
 sudo apt-get install - y libc-dev-bin libc6-dev linux-libc-dev manpages manpages-fr manpages-fr-extra manpages-fr-dev manpages-dev manpages-posix-dev xscreensaver xscreensaver-data ibus ibus-gtk ibus-gtk3 libibus-1.0 poppler-utils wkhtmltopdf
@@ -35,39 +34,12 @@ sudo apt-get update
 
 #Installation du microcode pour les processeurs intel
 sudo apt-get install - y intel-microcode
-=======
-sudo apt-get install xorg openbox xinit pcmanfm chromium-browser terminator notify-osd libnotify-bin numlockx
-sudo apt-get update
-sudo apt-get install libc-dev-bin libc6-dev linux-libc-dev manpages manpages-fr manpages-fr-extra manpages-fr-dev manpages-dev manpages-posix-dev xscreensaver xscreensaver-data ibus ibus-gtk ibus-gtk3 libibus-1.0 poppler-utils wkhtmltopdf
-sudo apt-get update
-sudo apt-get install lubuntu-restricted-addons lubuntu-restricted-extras linux-firmware-nonfree
-sudo apt-get update
-
-#Installation de logiciel complémentaire
-sudo apt-get install obconf lxappearance obmenu xarchiver alsa-utils gnome-alsamixer wicd menu menu-xdg gnome-menus openbox-xdgmenu gpicview cups gparted
-sudo apt-get update
-sudo apt-get install elementary-icon-theme nitrogen conky plank tint2 gufw compton deborphan synaptic apturl gdebi htop gmrun
-sudo apt-get update
-sudo apt-get install vim scrot gpicview evince gedit gksu vlc lubuntu-software-center language-selector-gnome tree ppa-purge git libreoffice-common
-sudo apt-get update
-
-#Instatallation de codec vidéo/audio + format de compression
-sudo apt-get install unace rar unrar unar p7zip-rar p7zip zip unzip arj libuu0 mpack sharutils uudeview gstreamer0.10-plugins-ugly gxine libdvdread4 icedax tagtool easytag id3tool lame libmad0 mpg321 libavcodec-extra xz-utils
-sudo apt-get update
-
-#Installation du microcode pour les processeurs intel
-sudo apt-get install intel-microcode
->>>>>>> 980a7f17bd006d73891ca2902da9ca4746442adf
 sudo apt-get update
 
 #Gestion de l'énergie, ajout du dépôt et installation
 sudo apt-add-repository ppa:linrunner/tlp
 sudo apt-get update
-<<<<<<< HEAD
 sudo apt-get install - y tlp tlp-rdw
-=======
-sudo apt-get install tlp tlp-rdw
->>>>>>> 980a7f17bd006d73891ca2902da9ca4746442adf
 sudo apt-get update
 sudo apt-get remove --purge network-manager-gnome
 sudo apt-get update
